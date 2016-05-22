9f57cf4766f5cba81a9dff91947fc9448e5fce5d
Jaakko Pallari <jkpl@lepovirta.org>
2016-05-22 17:54:26 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,19 +5,19 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.13"]
+                 [org.slf4j/slf4j-log4j12 "1.7.21"]
                  [log4j/log4j "1.2.17"
                   :exclusions [javax.mail/mail
                                javax.jms/jms
                                com.sun.jmdk/jmxtools
                                com.sun.jmx/jmxri]]
                  [stasis "2.3.0"]
-                 [optimus "0.18.4"]
+                 [optimus "0.18.5"]
                  [optimus-sass "0.0.3"]
                  [ring "1.4.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [clj-time "0.11.0"]
-                 [selmer "0.9.8"]]
+                 [selmer "1.0.4"]]
   :target-path "target/%s"
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler lepo.live/app
