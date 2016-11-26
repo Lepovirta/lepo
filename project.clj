80e0d500f67c1489d9352bb138e195f0bedd1740
Jaakko Pallari <jkpl@lepovirta.org>
2016-11-26 18:21:50 +0000

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://lepo.io/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.21"]
                  [log4j/log4j "1.2.17"
@@ -12,14 +12,14 @@
                                com.sun.jmdk/jmxtools
                                com.sun.jmx/jmxri]]
                  [stasis "2.3.0"]
-                 [optimus "0.18.5"]
+                 [optimus "0.19.0"]
                  [optimus-sass "0.0.3"]
-                 [ring "1.4.0"]
+                 [ring "1.5.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [clj-time "0.11.0"]
-                 [selmer "1.0.4"]]
+                 [clj-time "0.12.2"]
+                 [selmer "1.10.0"]]
   :target-path "target/%s"
-  :plugins [[lein-ring "0.9.7"]
+  :plugins [[lein-ring "0.10.0"]
             [lein-cloverage "1.0.9"]]
   :ring {:handler lepo.live/app
          :init lepo.live/app-init}
