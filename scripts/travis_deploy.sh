af43d068c7b8e949ba8918cf07b8ec2bcb1ed7f3
Jaakko Pallari <jkpl@lepovirta.org>
2016-11-27 03:11:36 +0000

    Find the Travis build directory

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -5,6 +5,8 @@ set -e
 SCRIPT_DIR=$(dirname "$0")
 DEPLOY="$SCRIPT_DIR/deploy.sh"
 
+cd "$TRAVIS_BUILD_DIR"
+
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     "$DEPLOY" production "$PRODUCTION_S3_BUCKET"
 else
