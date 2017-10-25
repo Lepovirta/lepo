1a73042d43df8820ee3e1b21e793a9b248afea93
Jaakko Pallari <jkpl@lepo.io>
2017-10-25 20:22:13 +0100

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -4,21 +4,21 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.23"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.slf4j/slf4j-log4j12 "1.7.25"]
                  [log4j/log4j "1.2.17"
                   :exclusions [javax.mail/mail
                                javax.jms/jms
                                com.sun.jmdk/jmxtools
                                com.sun.jmx/jmxri]]
                  [stasis "2.3.0"]
-                 [optimus "0.19.1"]
+                 [optimus "0.20.1"]
                  [optimus-sass "0.0.3"]
-                 [ring "1.5.1"]
+                 [ring "1.6.2"]
                  [org.clojure/data.xml "0.0.8"]
-                 [clj-time "0.13.0"]
-                 [selmer "1.10.6"]
-                 [hickory "0.7.0"]
+                 [clj-time "0.14.0"]
+                 [selmer "1.11.1"]
+                 [hickory "0.7.1"]
                  [hiccup "1.0.5"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :target-path "target/%s"
