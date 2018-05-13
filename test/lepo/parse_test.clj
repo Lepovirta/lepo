e4a40a87e018574c0a2cab759409d8baef8638e9
Jaakko Pallari <jkpl@lepo.group>
2018-05-13 17:44:51 +0300

    Remove a bunch of outdated tests

--- test/lepo/parse_test.clj
+++ test/lepo/parse_test.clj
@@ -8,9 +8,6 @@
 (def expected-parsed-pages
   [{:uri "/posts/2015-12-01-hello-world.html"
     :path "/posts/2015-12-01-hello-world.html"
-    :id "posts:2015-12-01-hello-world"
-    :date (t/date-time 2015 12 1)
-    :page-type :post
     :author-id :jsmith
     :description "cool story"
     :title "Hello World!"
@@ -18,9 +15,6 @@
     :content (list [:p {} "Hello World!"])}
    {:uri "/posts/2016-01-01-new-year.html"
     :path "/posts/2016-01-01-new-year.html"
-    :id "posts:2016-01-01-new-year"
-    :date (t/date-time 2016 1 1)
-    :page-type :post
     :author-id :jdoe
     :description "yay!"
     :title "New Year!"
@@ -28,26 +22,17 @@
     :content (list [:p {} "Happy New Year!"])}
    {:uri "/posts/2016-06-15-summer-time.html"
     :path "/posts/2016-06-15-summer-time.html"
-    :id "posts:2016-06-15-summer-time"
-    :date (t/date-time 2016 6 15)
-    :page-type :post
     :author-id :jdoe
     :title "Summer time!"
     :tags ["party"]
     :content (list [:p {} "so hot"])}
    {:uri "/team/jsmith/index.html"
     :path "/team/jsmith/index.html"
-    :id "team:jsmith:index"
-    :date nil
-    :page-type :author
     :description "my homepage"
     :content (list [:p {} "Hi, I'm John Smith!"])}
    {:uri "/zombo.html"
     :path "/zombo.html"
-    :id "zombo"
-    :date nil
     :title "ZOMBOCOM"
-    :page-type :normal
     :description "Welcome to ZOMBOCOM"
     :template "zombo"
     :content (list [:p {} "You can do anything in ZOMBOCOM!"])}])
