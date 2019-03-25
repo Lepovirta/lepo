fa81cb672b4a0e8b1e05efb0987e68a0a6f7b83a
Jaakko Pallari <jaakko.pallari@polarsquad.com>
2019-03-25 21:40:52 +0200

    BOOM!

--- src/lepo/resources.clj
+++
@@ -1,9 +0,0 @@
-(ns lepo.resources
-  (:require [clojure.java.io :as io]
-            [stasis.core :as stasis]))
-
-(def pages-dirname "pages")
-(def page-pattern #"\.html$")
-
-(defn raw-page-source []
-  (stasis/slurp-resources pages-dirname page-pattern))
--- src/lepo/template/normal.clj
+++
@@ -1,9 +0,0 @@
-(ns lepo.template.normal
-  (:require [lepo.template.html :as html]))
-
-(defn template
-  [conf]
-  (let [page (:page conf)
-        {title :title content :content} page]
-    (list [:header {} (html/heading 1 title)]
-          content)))
