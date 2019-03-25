5f5d871127fb73d43988dca82e91f8cc7cdba547
Jaakko Pallari <jaakko.pallari@polarsquad.com>
2019-03-25 21:40:52 +0200

    BOOM!

--- src/lepo/template/tag.clj
+++
@@ -1,8 +0,0 @@
-(ns lepo.template.tag
-  (:require [lepo.template.html :as html]))
-
-(defn template
-  [{page :page}]
-  (let [{posts :posts title :title} page]
-    (list [:header (html/heading 1 title)]
-          (html/post-list posts :default))))
