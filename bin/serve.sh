b39bb4cc29652dd3ebf3cf34b20aaf77cf3ede21
Jaakko Pallari <jaakko.pallari@polarsquad.com>
2019-03-31 21:30:58 +0300

    Auto-download on serve

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 cd "$(dirname "$0")/.."
+
+if [ ! -x bin/hugo ]; then
+    echo "Hugo not available. Downloading." >&2
+    ./bin/download_hugo.sh
+fi
 ./bin/hugo server
