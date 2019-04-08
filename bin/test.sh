78ce00f3a9e6da0afec6844d2ded5f07df2c9a6a
Jaakko Pallari <jkpl@lepovirta.org>
2019-04-08 21:46:37 +0300

    Testing using Lukki

---
+++ bin/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd "$(dirname "$0")/.."
+trap "kill 0" EXIT
+
+# Start live server
+./bin/serve.sh 2>/dev/null >&2 &
+
+# Wait until server has started
+curl -sfSL -4 --retry 10 --retry-connrefused --retry-delay 2 "http://localhost:1313/" >/dev/null
+
+# Test the site
+echo '{"urls": ["http://localhost:1313/"]}' | lukki
