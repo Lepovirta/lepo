26153013d80e406ce2c63ec94cd4e7e615a4ff06
Jaakko Pallari <jkpl@lepovirta.org>
2020-05-14 19:56:35 +0300

    ShellCheck on test cycle
    Make sure that all the shell scripts are validated through ShellCheck.
    
    Closes #53

--- bin/test.sh
+++ bin/test.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euox pipefail
 cd "$(dirname "$0")/.."
 trap "kill 0" EXIT
 
-# Start live server
+echo "Lint checks shell scripts" >&2
+shellcheck bin/*.sh
+
+echo "Starting test server" >&2
 ./bin/serve.sh 2>/dev/null >&2 &
 
-# Wait until server has started
+echo "Waiting for test server to become available..." >&2
 curl -sfSL -4 --retry 10 --retry-connrefused --retry-delay 2 "http://localhost:1313/" >/dev/null
 
-# Test the site
+echo "Run website tester" >&2
 echo '{
     "urls": ["http://localhost:1313/"],
     "ignoreRobotsTxt": true
